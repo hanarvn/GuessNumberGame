077e57379df24ae8127429ec455e46e6b10f85a2
Hana Arvin <hanaarvin2005@gmail.com>
2025-08-03 03:03:18 +0330

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int randomNumber(int n) {
+        return n * n - (3 * n) + 3;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("I have a number in my mind !\nPlease guess it : ");
+            int n = scanner.nextInt();
+            int x = randomNumber(n);
+            boolean flag = false;
+            while (!flag) {
+                if (x == n) {
+                    System.out.println("******* THAT WAS RIGHT *******\nAgain :\n");
+                    flag = true;
+                } else {
+                    if (n > x) {
+                        System.out.println("Your nuber is larger than mine !\nRetry !");
+                    } else {
+                        System.out.println("Your number is smaller than mine !\nRetry !");
+                    }
+                    n = scanner.nextInt();
+                }
+
+
+            }
+        }
+    }
+}
